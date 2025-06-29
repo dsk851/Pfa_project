df7b2ddd2293d755b2647b780eca919f7a6799f3
Espoir <dskd2805@gmail.com>
2025-06-29 22:55:52 +0100

    deactivation of the learn more button in the infosection

--- car_marketplace/src/components/InfoSection.jsx
+++ car_marketplace/src/components/InfoSection.jsx
@@ -3,35 +3,38 @@ import { Button } from './ui/button'
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 function InfoSection() {
+  // Pour désactiver le bouton, ajoutez la prop 'disabled'
   return (
     <section>
-  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 mt-10">
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
-      <div>
-        <div className="max-w-lg md:max-w-none">
-          <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h2>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 mt-10">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
+          <div>
+            <div className="max-w-lg md:max-w-none">
+              <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
 
-          <p className="mt-4 text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloremque saepe
-            architecto maiores repudiandae amet perferendis repellendus, reprehenderit voluptas
-            sequi.
-          </p>
+              <p className="mt-4 text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloremque saepe
+                architecto maiores repudiandae amet perferendis repellendus, reprehenderit voluptas
+                sequi.
+              </p>
+            </div>
+            <Button className="mt-6" disabled>
+              Learn more <FaLongArrowAltRight className="text-white text-[20px] ml-2" />
+            </Button>
+          </div>
+
+          <div>
+            <img
+              src="https://www.bmw.ma/content/dam/bmw/common/all-models/m-series/series-overview/bmw-m-series-seo-overview-ms-08.jpg"
+              className="rounded"
+              alt=""
+            />
+          </div>
         </div>
-            <Button className="mt-6">Learn more <FaLongArrowAltRight className="text-white text-[20px] ml-2" /></Button>
       </div>
-
-      <div>
-        <img
-          src="https://www.bmw.ma/content/dam/bmw/common/all-models/m-series/series-overview/bmw-m-series-seo-overview-ms-08.jpg"
-          className="rounded"
-          alt=""
-        />
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
   )
 }
 
